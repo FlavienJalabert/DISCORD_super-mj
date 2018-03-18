e162924420545d8f4f09694d7adbd1eae3b21e5e
FlavienJalabert <32567045+FlavienJalabert@users.noreply.github.com>
2018-03-18 22:35:56 +0100

    bot

--- commands.js
+++ commands.js
@@ -6,7 +6,7 @@ var commands = [
 
 module.exports = {
 	request : function(requestedCmd) {
-		if (typeof commands.find(x => x.cmd === requestedCmd) !== "undifined") {
+		if (typeof commands.find(x => x.cmd === requestedCmd) !== "undefined") {
 			return commands.find(x => x.cmd === requestedCmd).response;
 		}else return false
 	}
