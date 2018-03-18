4f9e47dbd4017fe29ed5ded8f19b4c0fb540c4c6
FlavienJalabert <32567045+FlavienJalabert@users.noreply.github.com>
2018-03-18 22:41:27 +0100

    bot

--- commands.js
+++ commands.js
@@ -1,7 +1,7 @@
 var commands = [
 	{cmd: 'ping', response: 'Pong!'},
 	{cmd: 'bonjour', response: 'Coucou toi!'},
-	{cmd: 'quel est la couleur du ciel ?', response: 'Bleu, même si je n'ai pas d'yeux pour le voir mon programmateur m\'a dit que bleu est la couleur du ciel''},
+	{cmd: 'quel est la couleur du ciel ?', response: 'Bleu, même si je n\'ai pas d\'yeux pour le voir mon programmateur m\'a dit que bleu est la couleur du ciel''},
 	{cmd: 'pourquoi la vie ?', response: 'Parce que les humains vivent ils se questionnent souvent à ce sujet, malheureusement, n\'étant pas humaine je ne peux vraiment vous répondre, désolée...''},
 	{cmd: 'cerises', response: 'Ma petite cerise'}
 ];
