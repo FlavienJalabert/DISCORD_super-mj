41487b024f5ee9d94047cb683b8f886fa613d552
FlavienJalabert <32567045+FlavienJalabert@users.noreply.github.com>
2018-03-18 21:09:47 +0100

    bot

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ bot.on('message', message => {
 	if(message.author.bot) return;
     if (message.content.substring(0, 1) == '$') {
         var args = message.content.substring(1).split(' ');
-        var cmd = args[0]);
+        var cmd = args[0];
         args = args.splice(1);
         switch(cmd) {
             case 'ping':
