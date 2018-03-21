ffc6674e8fabdf21b37d6c8ca87f384e4a12082f
FlavienJalabert <32567045+FlavienJalabert@users.noreply.github.com>
2018-03-21 21:12:21 +0100

    talkative mode

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const Commands = require("./commands.js");
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
+var isTalkative = process.env.TALKATIVITY;
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}`);
@@ -14,21 +15,24 @@ bot.on('message', message => {
 	
 	var cmd = message.content;
 	
-	if(cmd.indexOf(process.env.PREFIX) !== 0) {
-		
+	if(cmd.indexOf(process.env.PREFIX) == -1  && isTalkative == true) {
 		if(Commands.request(cmd)) {
 				message.channel.send(Commands.request(cmd));
 		}else {
 			console.log("No Matchs for : "+cmd);
 		}
 	}else {
-		var args = cmd.slice(process.env.PREFIX.length).trim().split(/ +/g);
-		const command = args.shift().toLowerCase();
-	
+		var args = cmd.substring(1, cmd.length);
+		const command = args.toLowerCase();
 	
 	if(command == "play") {
 		//do
 		console.log("do play")
+	}
+	
+	if(command == "talkative") {
+		isTalkative = !isTalkative;
+		message.channel.send("talkative set to " + isTalkative);
 	}
 	
 	
