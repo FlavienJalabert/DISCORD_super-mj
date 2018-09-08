e9e2f73cd2eb619dd935481da0bb6738a41b9b7c
FlavienJalabert <32567045+FlavienJalabert@users.noreply.github.com>
2018-09-08 11:25:11 +0200

    upgrade 8.2

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
-const Discord = require("discord.js");
-const Commands = require("./src/commands.js");
-const CommonFunction = require("./src/commonFunction.js");
-const fs = require("fs");
+const Discord=require("discord.js");
+const Commands=require("./src/commands.js");
+const CommonFunction=require("./src/commonFunction.js");
+const fs=require("fs");
 
 // Initialize Discord Bot
-var bot = new Discord.Client();
-var isTalkative = process.env.TALKATIVITY;
-var shiritori = false;
+var bot=new Discord.Client();
+var isTalkative=process.env.TALKATIVITY;
+var shiritori=false;
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}`);
@@ -18,7 +18,7 @@ bot.on('ready', () => {
 bot.on('message', message => {
 	if(message.author.bot) return;
 	
-	var cmd = message.content;
+	var cmd=message.content;
 	
 	if(cmd.indexOf(process.env.PREFIX) == -1  && isTalkative == true) {
 		if(Commands.request(cmd)) {
@@ -27,53 +27,53 @@ bot.on('message', message => {
 			console.log("No Matchs for : "+cmd);
 		}
 	}else {
-		const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
-		const command = args.shift().toLowerCase();
+		const args=message.content.slice(process.env.PREFIX.length).trim().split(/ +/g);
+		const command=args.shift().toLowerCase();
 	
 	if(command == "talkative") {
-		isTalkative = !isTalkative;
+		isTalkative=!isTalkative;
 		message.channel.send("talkative set to " + isTalkative);
 	}
 	
 	if (command === "shiritori" && shiritori != true) {
-		shiritori = true;
-		const channel = message.channel;
+		shiritori=true;
+		const channel=message.channel;
 		channel.send("How many members wants to play ?");
-        var Collector1 = channel.createCollector(message => message.author.bot === false);
+        var Collector1=channel.createCollector(message => message.author.bot === false);
         Collector1.on('collect', message => {
-			var nop = Number(message.content);
+			var nop=Number(message.content);
 			if (nop <= 1 || typeof nop != 'number') {
 				channel.send("Wrong argument, stopping the game");
-				shiritori = false;
+				shiritori=false;
 			}else {
-				var player = Math.floor(Math.random()*nop+1);
+				var player=Math.floor(Math.random()*nop+1);
 				channel.send("The first player is player " + player);
 				Collector1.stop('game started');
-				var Collector2 = channel.createCollector(message => message.author.bot === false);
+				var Collector2=channel.createCollector(message => message.author.bot === false);
 				Collector2.on("collect", message => {
-					let lastWord = message.content;
+					let lastWord=message.content;
 					Collector2.stop('first word set');
-					var Collector = new Discord.MessageCollector(channel, message => message.author.bot === false);
-					player = CommonFunction.nextPlayer(player, nop);
+					var Collector=new Discord.MessageCollector(channel, message => message.author.bot === false);
+					player=CommonFunction.nextPlayer(player, nop);
 					channel.send("Next player is player " + player);
 					Collector.on("collect", message => {
 						if(message.content == "stop") {
-							var winners = new CommonFunction.getWinners(player, nop);
+							var winners=new CommonFunction.getWinners(player, nop);
 							channel.send("Player " + player + ", you loose by "+"give up"+", nice tried ! \nPlayer(s) " +winners.join()+ " won the round!");
 							Collector.stop('game end');
 							channel.send("Game ended.");
-							shiritori = false;
+							shiritori=false;
 						}else{
-							var comp = CommonFunction.arbitrator(lastWord, message.content);
+							var comp=CommonFunction.arbitrator(lastWord, message.content);
 							if (comp[0] == 1) {
-								var winners = new CommonFunction.getWinners(player, nop);
+								var winners=new CommonFunction.getWinners(player, nop);
 								channel.send("Player " + player + ", you loose by "+comp[1]+", nice tried ! \nPlayer(s) " +winners.join()+ " won the round!");
 								Collector.stop('game end');
 								channel.send("Game ended.");
-								shiritori = false;
+								shiritori=false;
 							}else{
-								lastWord = message.content;
-								player = CommonFunction.nextPlayer(player, nop);
+								lastWord=message.content;
+								player=CommonFunction.nextPlayer(player, nop);
 								channel.send("Next player is player " + player);
 								if(Math.random() <= Math.random()) {channel.send("You can end your turn anytime by taping 'stop'. But it will be counted as a give up !");}
 							}
@@ -91,7 +91,7 @@ bot.on('message', message => {
 	}
 	
 	if(command == "help") {
-		var help = new Array();
+		var help=new Array();
 		help.push("talkative: Set the talkativity of the bot (true: will respond to any message, false: be quiet)");
 		help.push("shiritori: Start a new game of shiritori");
 		help.push("game: Set status of the bot to the defined game");
